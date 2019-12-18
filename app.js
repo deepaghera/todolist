02b7bad5fe5313fa24c77feaa235638b9f15bdb2
deepaghera <deepaghera2000@gmail.com>
2019-12-18 13:56:29 +0530

    some modification

--- app.js
+++ app.js
@@ -6,35 +6,16 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-   var today = new Date();
-   var currentDay=  today.getDay();
-   var day = "";
-   switch ( currentDay ) {
-       case 0:
-           day = "Sunday";
-           break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-           day = "Thursday";
-            break;
-        case 5:
-           day = "Friday";
-            break;
-        case 6:
-            day = "Saturday"
-       default:
-           console.log("Error: currentDay value =" + currentDay);
-           break;
-   }
-   res.render("list",{kindOfDay : day} );
+    var today = new Date();
+    var option = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+    }
+    var day = today.toLocaleDateString("en-US", option);
+    res.render("list", {
+        kindOfDay: day
+    });
 });
 
 app.listen(3000, () => {
