24e25279dea68d6f2f1f8565bd1ab3daebb6da6f
deepaghera <deepaghera2000@gmail.com>
2019-12-18 16:48:57 +0530

    add css file

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var Items = ["Buy food","Cook Food","Eat Food"]; 
 
 app.get('/', (req, res) => {
     var today = new Date();
@@ -14,10 +20,15 @@ app.get('/', (req, res) => {
     }
     var day = today.toLocaleDateString("en-US", option);
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newItems: Items,
     });
 });
-
+app.post('/', (req, res) => {
+     Item = req.body.newitem;
+     Items.push(Item);
+    res.redirect("/");
+});
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
 });
